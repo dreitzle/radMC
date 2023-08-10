4ee079514665d8fa99b855379440fd0e408ef682
David Hevisov <david.hevisov@ilm.uni-ulm.local>
2023-08-10 09:56:57 +0200

    Cleaned up Python host code and corrected analytic rad example for reference.

--- python/config_loader.py
+++ python/config_loader.py
@@ -1,61 +1,60 @@
 import json
 
-class config:   
+
+class config:
 
     def __init__(self, file_path):
-        
+
         try:
-            
             print(f"Trying to open config from path: {file_path}...", end=" ")
-            
-            with open(file_path) as file:  
+
+            with open(file_path) as file:
                 self.data = json.load(file)
-                
+
             print("Done!")
-                
-        
-        except:
-            
+
+        except Exception:
             print("No config file found, creating standard config")
-            
-            
-            self.data =  {
-                "mc_config": {
-                  "photons": 10000000,
-                  "n_scat_pthread": 200,
-                  "n_scat_max": 100000,
-                  "n_phi": 1,
-                  "n_costheta": 180
-                  },
-                  
-                "opencl_config": {
-                    "platform": 0,
-                    "devices": [0],
-                    "threads": 65536
-                    },
-                    
-                "sim_parameters": {
-                    "mua": 0.1,
-                    "mus": 0.1,
-                    "g": 0.0
-                }
-            }
+
+            self.data = {
+                            "mc_config": {
+                                "photons": 1e5,
+                                "n_scat_pthread": 100,
+                                "n_scat_max": 100000,
+                                "n_phi": 1,
+                                "n_costheta": 50
+                            },
+                            "opencl_config": {
+                                "platform": 0,
+                                "devices": [
+                                    0
+                                ],
+                                "threads": 4096
+                            },
+                            "sim_parameters": {
+                                "mua": 0.01,
+                                "mus": 5.0,
+                                "g": 0.8,
+                                "n1": 1.0,
+                                "n2": 1.4,
+                                "theta_ls": 0
+                            }
+                        }
 
             with open('config.json', 'w') as file:
                 json.dump(self.data, file, indent=4)
-  
-class ocl_config:   
-    
+
+
+class ocl_config:
+
     def __init__(self):
-        
+
         self.build_options = []
-        
+
     def add_path(self, include_path):
-        
-        self.build_options.append("-I " + include_path)
-        
+
+        self.build_options.append(f"-I {include_path}")
+
     def add_build_define(self, MACRO):
-        
-        self.build_options.append("-D " + MACRO)
-        
-        
+
+        self.build_options.append(f"-D {MACRO}")
--- python/structs.py
+++ python/structs.py
@@ -6,69 +6,61 @@ Created on Sat Jun 17 17:56:14 2023
 """
 
 import numpy as np
-import ctypes
 
-class Photons:   
+
+class Photons:
 
     def __init__(self, N_threads):
 
         # Define the struct-like Photon class
 
         photon_type = np.dtype([
-        ('dir', np.float32, 4),
-        ('zpos', np.float32),
-        ("weight", np.float32),
-        ("scat_counter", np.uint32)
-        ])
+            ('dir', np.float32, 4),
+            ('zpos', np.float32),
+            ("weight", np.float32),
+            ("scat_counter", np.uint32)
+            ])
 
         self.photons = np.zeros(N_threads, dtype=photon_type)
-        
+
+
 class Sim_Parameters:
-    
-    def __init__(self,cfg):
-        
+
+    def __init__(self, cfg):
+
         self.mua = cfg["sim_parameters"]["mua"]
-        self.mus = cfg["sim_parameters"]["mus"] 
+        self.mus = cfg["sim_parameters"]["mus"]
         self.g = cfg["sim_parameters"]["g"]
         self.n1 = cfg["sim_parameters"]["n1"]
         self.n2 = cfg["sim_parameters"]["n2"]
         self.theta_ls = cfg["sim_parameters"]["theta_ls"]
-        
+
     def calc_start_dir(self):
-        
-        """ 
+        """
         Calculate initial direction after refraction.
         """
-        
+
         self.cost_start = np.cos(np.deg2rad(self.theta_ls))
-        
-        if (self.theta_ls > 0.000001): # light source is not perpendicular to surface
-            
+
+        if (self.theta_ls > 0.000001):  # light source is not perpendicular to surface
+
             n_ratio = self.n1/self.n2
-            
+
             self.cost_start = self.cost_start*n_ratio - (n_ratio*self.cost_start - np.sqrt(1 - n_ratio*n_ratio*(1 - self.cost_start*self.cost_start)))
-            
+
     def calc_R_fresnel(self):
-        
+
         n = self.n2/self.n1
         mu = self.cost_start
         mu_crit = np.sqrt(n*n-1)/n if n > 1.0 else 0.0
-        
+
         if (mu > mu_crit):
-        
+
             mu0 = np.sqrt(1.0-n*n*(1.0-mu*mu))
             f1 = (mu-n*mu0)/(mu+n*mu0)
             f2 = (mu0-n*mu)/(mu0+n*mu)
             self.R = 0.5*f1*f1+0.5*f2*f2
-        
-        else: self.R = 1 # total reflexion
 
-        
+        else:
 
-class Tyche_i_state(ctypes.Structure):
-    
-    _fields_ = [("a", ctypes.c_uint32),
-                ("b", ctypes.c_uint32),
-                ("c", ctypes.c_uint32),
-                ("d", ctypes.c_uint32)]
-    
+            self.R = 1  # total reflexion
