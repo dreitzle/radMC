24484124382a072298d11a2ec453a811eaf7b10f
Dominik Reitzle <dominik.reitzle@ilm-ulm.de>
2023-08-18 18:03:31 +0200

    Add some tests for semi-infinite medium.

---
+++ cpp/tests/test_semi_lf.cpp
@@ -0,0 +1,57 @@
+#define BOOST_TEST_MODULE mc_semi_lf
+#include <boost/test/unit_test.hpp>
+
+#include "config.hpp"
+#include "clsim.hpp"
+#include "netcdf_interface_mc.hpp"
+
+/* File with reference values (netcdf) */
+#define REF_FILE "data_test_semi1.nc"
+
+/* Required precision in percent*/
+#define PREC 1.0
+
+/* Test Legendre algorithms with double precision */
+BOOST_AUTO_TEST_SUITE( test_lf )
+
+    /* Test double vector */
+    BOOST_AUTO_TEST_CASE( case1_lf )
+    {
+        /* reference data */
+        netcdf_interface ncfile(std::string(DATA_PATH).append(REF_FILE));
+        ncfile.open_file(NC_NOWRITE);
+
+        std::vector<cl_float> cost_points;
+        std::vector<cl_float> phi_points;
+        std::vector<cl_float> res_r_ref;
+
+        ncfile.get_variable_vector("cost", cost_points);
+        ncfile.get_variable_vector("phi", phi_points);
+        ncfile.get_variable_vector("radiance", res_r_ref);
+
+        ncfile.close_file();
+
+        /* MC Simulation */
+        Config config(std::string(DATA_PATH).append("config_test_semi1.json"));
+
+        const unsigned int n_costheta = cost_points.size();
+        const unsigned int n_phi = phi_points.size();
+        const unsigned int detsize = n_costheta*n_phi;
+
+        CLsim sim(config.ocl_config.platform, config.ocl_config.devices[0], "-DUSE_LF");
+
+        sim.set_points(cost_points,phi_points);
+        sim.create_buffers(config);
+        sim.seed_rng(config);
+
+        sim.run(config);
+        const auto& [res_r, res_t] = sim.get_result();
+
+        for(unsigned int i = 0; i < detsize; i++)
+            BOOST_CHECK_CLOSE( res_r[i], res_r_ref[i] , PREC);
+
+        for(unsigned int i = 0; i < detsize; i++)
+            BOOST_CHECK_SMALL( res_t[i], PREC);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
